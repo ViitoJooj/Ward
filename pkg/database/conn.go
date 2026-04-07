fd593f482411b45e4d0583a43da83cf34803a7e5
hugaojanuario <hugo.santariosi@siplan.com.br>
2026-04-07 09:37:33 -0300

    fix: correcao no retorno de erros uqe vinham do banco, configurada a lib sqlite3 no conn.go

--- pkg/database/conn.go
+++ pkg/database/conn.go
@@ -1,39 +1,18 @@
 package database
 
 import (
-        "database/sql"
+	"database/sql"
 
-        "github.com/golang-migrate/migrate/v4"
-        "github.com/golang-migrate/migrate/v4/database/sqlite3"
-        _ "github.com/golang-migrate/migrate/v4/source/file"
-        _ "github.com/mattn/go-sqlite3"
-  )
+	_ "github.com/mattn/go-sqlite3"
+)
 
 var DB *sql.DB
 
 func Conn() error {
-    var err error
-    DB, err = sql.Open("sqlite3", "./database.db")
-    if err != nil {
-            return err
-    }
-
-    driver, err := sqlite3.WithInstance(DB, &sqlite3.Config{})
-    if err != nil {
-        return err
-    }
-
-    m, err := migrate.NewWithDatabaseInstance(
-            "file://db/migrations",
-            "sqlite3", driver,
-    )
-    if err != nil {
-        return err
-    }
-
-    if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-        return err
-    }
-
-    return nil
+	var err error
+	DB, err = sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		return err
+	}
+	return nil
 }
